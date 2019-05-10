431223d106c6beed87e3bb3fffcacfecc28b376f
Arnout Engelen <arnout@bzzt.net>
2019-05-10 08:03:16 +0200

    Create gh-pages branch from Help

--- lib/globals.nas
+++
@@ -1,5 +0,0 @@
-# This is just an empty namespace for sharing global variables between modules.
-# Import with import("globals","*");
-var globals={};
-EXPORT=["globals"];
-
--- src/setup_osx_env.sh
+++
@@ -1,7 +0,0 @@
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/opt/local/lib/pkgconfig:/usr/lib/pkgconfig
-export MACOSX_DEPLOYMENT_TARGET=10.4
-export CFLAGS="-O2"
-
-#export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${PWD}/pkgconfig
-#mkdir pkgconfig 2>/dev/null
-#cp /Library/Frameworks/*.framework/Resources/dev/lib/pkgconfig/*.pc pkgconfig/
